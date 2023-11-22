3f0c87bd3d9176f446895b4726e79b029b17ee03
xuandd <103272537+xuan3chan@users.noreply.github.com>
2023-11-22 16:13:06 +0700

    nbjb

--- models/connectDatabase.js
+++ models/connectDatabase.js
@@ -1,14 +1,13 @@
 const sql = require('msnodesqlv8');
+require('dotenv').config();
 
-const connectionString = 'server=.;Database=QUANLYBDS_TEAM3;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}';
+const connectionString = `server=${process.env.DB_SERVER};Database=QUANLYBDS_TEAM3;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
 
 const conn = sql.open(connectionString, (err) => {
     if (err) {
         console.log(err);
     } else {
         console.log('Database connection successful!');
-     
     }
 });
-
-module.exports = conn;
+module.exports = conn; 

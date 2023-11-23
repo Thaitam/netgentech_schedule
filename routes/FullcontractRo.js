e0609a99898faa4e2d45ad531f535ccc96c00f43
xuandd <103272537+xuan3chan@users.noreply.github.com>
2023-11-24 03:38:17 +0700

    getfullcontract view

--- routes/FullcontractRo.js
+++ routes/FullcontractRo.js
@@ -4,17 +4,7 @@ const router = express.Router();
 const fullcontractController = require('../controllers/FullcontractController');
 
 router.post('/add', fullcontractController.addFullcontract);
+router.get('/getall', fullcontractController.getFullcontract);
 
-router.get('/getall', (req, res) => {
-  fullcontractController.getFullcontract((err, data) => {
-    if (err) {
-      // handle error
-      console.log(err);
-    } else {
-      console.log(data);
-      res.render('../views/components/fullContract/index.ejs', { contracts: data });
-    }
-  });
-});
 
 module.exports = router;

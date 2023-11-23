0e2c8726baefdef48937f6827402c6ca8a367d6f
Thaitam <letranthaitam3000@gmail.com>
2023-11-24 01:30:45 +0700

    Remove unnecessary code and console.log error in FullcontractRo.js and index.ejs

--- routes/FullcontractRo.js
+++ routes/FullcontractRo.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const fullcontractController = require('../controllers/FullcontractController');
 
 router.post('/add', fullcontractController.addFullcontract);
-router.get('/getall', fullcontractController.getFullcontract);
 
 router.get('/getall', (req, res) => {
   fullcontractController.getFullcontract((err, data) => {
     if (err) {
       // handle error
+      console.log(err);
     } else {
       console.log(data);
       res.render('../views/components/fullContract/index.ejs', { contracts: data });

7a03cff4ec86317442c0f4ab554e42a474bb0b26
Thaitam <letranthaitam3000@gmail.com>
2023-11-23 17:05:14 +0700

    Add express-ejs-layouts and update routes and views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // app.js
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
 const app = express();
 const port = 8888;
 const path = require('path');
@@ -7,16 +8,26 @@ const FullcontractRo = require('./routes/FullcontractRo');
 const PropertyRo = require('./routes/PropertyRo');
 app.use(express.json());
 
-
+app.use(express.static('public'))
+// app.use('/img', express.static(__dirname + 'public/img'))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.send('server is running');
 });
+
+app.use(expressLayouts)
+app.set('layout', './layout/managementLayout')
+app.set('view engine', 'ejs')
+
 //render
 app.get('/home', (req, res) => {
-  res.render('home');
+  res.render('index' , { title : 'Homepage'});
+});
+
+app.get('/addContract', (req, res) => {
+  res.render('./components/addContract/index.ejs' , { title : 'addContract'});
 });
 
 //routes

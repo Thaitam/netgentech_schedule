074a0ff89a59b6c968fe18e65c6c67c5637f0e76
xuandd <103272537+xuan3chan@users.noreply.github.com>
2023-11-25 01:05:39 +0700

    Refactor FullcontractController.js to check for existing contracts before adding a new one

--- controllers/FullcontractController.js
+++ controllers/FullcontractController.js
@@ -2,49 +2,72 @@ const database = require("../models/connectDatabase");
 
 class FullcontractController {
   async addFullcontract(req, res) {
-      const {
-        Customer_Name,
-        Year_Of_Birth,
-        SSN,
-        Customer_Address,
-        Mobile,
-        Property_ID,
-        Date_Of_Contract,
-        Price,
-        Deposit,
-        Remain,
-        Status,
-      } = req.body;
+    const {
+      Customer_Name,
+      Year_Of_Birth,
+      SSN,
+      Customer_Address,
+      Mobile,
+      Property_ID,
+      Date_Of_Contract,
+      Price,
+      Deposit,
+      Remain,
+      Status,
+    } = req.body;
 
-      //check missing field
-      if (!Customer_Name || !Year_Of_Birth || !SSN || !Property_ID || !Status) {
-        return res.status(400).json({ error: "Missing field" });
-      }
-
-      const query = `INSERT INTO Full_Contract (Customer_Name, Year_Of_Birth, SSN, Customer_Address, Mobile, Property_ID, Date_Of_Contract, Price, Deposit, Remain, Status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-      const values = [
-        Customer_Name,
-        Year_Of_Birth,
-        SSN,
-        Customer_Address,
-        Mobile,
-        Property_ID,
-        Date_Of_Contract, // No need to format the date
-        Price,
-        Deposit,
-        Remain,
-        Status
-      ];
-
-      try {
-        await database.query(query, values);
-        return res.status(201).json({ message: "Full contract added successfully" });
-      } catch (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
+    //check missing field
+    if (!Customer_Name || !Year_Of_Birth || !SSN || !Property_ID || !Status) {
+      return res.status(400).json({ error: "Missing field" });
     }
+
+    // Check if a contract with the given Property_ID exists
+    const queryCheck = `SELECT * FROM Full_Contract WHERE Property_ID = ?`;
+
+    try {
+      const result = await new Promise((resolve, reject) => {
+        database.query(queryCheck, [Property_ID], (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+  
+      if (Array.isArray(result) && result.length > 0) {
+        return res.status(400).json({ error: "A contract for this property already exists" });
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  
+
+    const query = `INSERT INTO Full_Contract (Customer_Name, Year_Of_Birth, SSN, Customer_Address, Mobile, Property_ID, Date_Of_Contract, Price, Deposit, Remain, Status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const values = [
+      Customer_Name,
+      Year_Of_Birth,
+      SSN,
+      Customer_Address,
+      Mobile,
+      Property_ID,
+      Date_Of_Contract, // No need to format the date
+      Price,
+      Deposit,
+      Remain,
+      Status
+    ];
+
+    try {
+      await database.query(query, values);
+      return res.status(201).json({ message: "Full contract added successfully" });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
 
   async getFullcontract(req, res) {
       const query = `SELECT * FROM Full_Contract`;

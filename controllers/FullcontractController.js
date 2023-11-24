a2468b8e789a362cb739431d8cd8e8f0db05fc99
xuandd <103272537+xuan3chan@users.noreply.github.com>
2023-11-25 00:20:29 +0700

    Fix date formatting in FullcontractController and add missing Date_Of_Contract field in addContract form

--- controllers/FullcontractController.js
+++ controllers/FullcontractController.js
@@ -2,60 +2,72 @@ const database = require("../models/connectDatabase");
 
 class FullcontractController {
   async addFullcontract(req, res) {
-    const {
-      Customer_Name,
-      Year_Of_Birth,
-      SSN,
-      Customer_Address,
-      Mobile,
-      Property_ID,
-      Date_Of_Contract,
-      Price,
-      Deposit,
-      Remain,
-      Status,
-    } = req.body;
+      const {
+        Customer_Name,
+        Year_Of_Birth,
+        SSN,
+        Customer_Address,
+        Mobile,
+        Property_ID,
+        Date_Of_Contract,
+        Price,
+        Deposit,
+        Remain,
+        Status,
+      } = req.body;
 
-    //check missing field
-    if (!Customer_Name || !Year_Of_Birth || !SSN || !Property_ID || !Status) {
-      return res.status(400).json({ error: "Missing field" });
-    }
+      //check missing field
+      if (!Customer_Name || !Year_Of_Birth || !SSN || !Property_ID || !Status) {
+        return res.status(400).json({ error: "Missing field" });
+      }
 
-    const query = `INSERT INTO Full_Contract (Customer_Name, Year_Of_Birth, SSN, Customer_Address, Mobile, Property_ID, Date_Of_Contract, Price, Deposit, Remain, Status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+      const query = `INSERT INTO Full_Contract (Customer_Name, Year_Of_Birth, SSN, Customer_Address, Mobile, Property_ID, Date_Of_Contract, Price, Deposit, Remain, Status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    const values = [
-      Customer_Name,
-      Year_Of_Birth,
-      SSN,
-      Customer_Address,
-      Mobile,
-      Property_ID,
-      Date_Of_Contract ? moment(Date_Of_Contract, "DD-MM-YYYY").format("YYYY-MM-DD") : null,
-      Price,
-      Deposit,
-      Remain,
-      Status
-    ];
+      const values = [
+        Customer_Name,
+        Year_Of_Birth,
+        SSN,
+        Customer_Address,
+        Mobile,
+        Property_ID,
+        Date_Of_Contract, // No need to format the date
+        Price,
+        Deposit,
+        Remain,
+        Status
+      ];
 
-    try {
-      await database.query(query, values);
-      return res.status(201).json({ message: "Full contract added successfully" });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
-
-  async getFullcontract(req, res) {
-    const query = `SELECT * FROM Full_Contract`;
-    database.query(query, (err, result) => {
-      if (err) {
+      try {
+        await database.query(query, values);
+        return res.status(201).json({ message: "Full contract added successfully" });
+      } catch (err) {
         console.error(err);
         return res.status(500).json({ error: "Internal Server Error" });
       }
-      return res.status(200).json(result);
-    });
-  }
+    }
+
+  async getFullcontract(req, res) {
+      const query = `SELECT * FROM Full_Contract`;
+      database.query(query, (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        const formattedResult = result.map(contract => {
+          // Convert Date_Of_Contract to a Date object
+          const date = new Date(contract.Date_Of_Contract);
+
+          // Format the date as YYYY-MM-DD
+          const formattedDate = date.toISOString().split('T')[0];
+
+          // Replace the original Date_Of_Contract with the formatted date
+          return { ...contract, Date_Of_Contract: formattedDate };
+        });
+
+        return res.status(200).json(formattedResult);
+      });
+    }
 
   async getFullContractById(req, res) {
     const { id } = req.params;
